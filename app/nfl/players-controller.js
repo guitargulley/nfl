765e3eb8a7ab665f83f3febfd7586f17ab7c0e20
Brandon Gulley <brandongulley@Brandons-MacBook-Pro.local>
2017-11-03 23:08:06 -0600

    initial commit

---
+++ app/nfl/players-controller.js
@@ -0,0 +1,127 @@
+var PlayerController = function () {
+  var loading = true; //Start the spinner
+  var playerService = new PlayerService(ready);
+  var filteredPlayers = {}
+  var page = 1
+  
+  
+  
+  
+  function ready() {
+    loading = false; //stop the spinner
+
+    //Now that all of our player data is back we can safely setup our bindings for the rest of the view.
+  }
+
+
+  this.getPlayersByTeam = function getPlayersByTeam(e) {
+    // debugger
+    e.preventDefault();
+    var teamName = e.target.teamName.value;
+    filteredPlayers = playerService.getPlayersByTeam(teamName)
+    console.log(filteredPlayers)
+    document.forms["filter-position-form"].reset()
+    document.forms["filter-lastName-form"].reset()
+    draw()
+  }
+  this.setPage = function setPage(num){
+    page = num
+    draw()
+  }
+
+  this.getPlayersByPosition = function getPlayersByPosition(e) {
+    e.preventDefault();
+    var position = e.target.position.value;
+    filteredPlayers = playerService.getPlayersByPosition(position)
+    console.log(filteredPlayers)
+    document.forms["filter-team-form"].reset()
+    document.forms["filter-lastName-form"].reset()
+    draw()
+
+  }
+  this.addToRoster = function addToRoster(id){
+    playerService.addToRoster(id, drawMine)
+  }
+
+  this.getPlayersByName = function getPlayersByName(e){
+    e.preventDefault();
+    var lastName = e.target.lastName.value;
+    // debugger
+    filteredPlayers = playerService.getPlayersByName(lastName)
+    console.log(filteredPlayers)
+    document.forms["filter-position-form"].reset()
+    document.forms["filter-team-form"].reset()
+  
+    draw()
+  }
+
+  this.displayNflRoster = function displayNflRoster(){
+    document.getElementById('my-roster-div').classList.add('hidden');
+    document.getElementById('my-roster').classList.add('hidden');
+    document.getElementById('nfl-roster-div').classList.remove('hidden') 
+    document.getElementById('playerList').classList.remove('hidden') 
+    
+    
+  }
+  this.displayMyRoster = function displayMyRoster(){
+    document.getElementById('nfl-roster-div').classList.add('hidden')
+    document.getElementById('playerList').classList.add('hidden') 
+    document.getElementById('my-roster-div').classList.remove('hidden') 
+    document.getElementById('my-roster').classList.remove('hidden'); 
+    
+  }
+
+  function draw() {
+    var template = ''
+    var currentArr = filteredPlayers[page]
+
+    for (var i = 0; i < currentArr.length; i++) {  
+        var player = currentArr[i];
+        template += 
+        `
+        <div class="col-xs-3 text-center players">
+          <img src="${player.playerImg}" alt="">
+          <h3>${player.firstName} ${player.lastName}</h3>
+          <h4>${player.team}</h4>
+          <h4>${player.position}</h4>
+          <button onclick="app.controllers.playerController.addToRoster(${player.id})">Add to roster</button>
+        </div>
+        `     
+    }
+    var pageTemplate = ''
+    for (var i = 1; i <= Object.keys(filteredPlayers).length; i++) {
+      pageTemplate += `
+      <button onclick="app.controllers.playerController.setPage(${i})">${i}</button>
+      
+      `
+      
+    }
+    document.getElementById('playerList').innerHTML = template
+    document.getElementById('pages').innerHTML = pageTemplate
+  }
+
+
+  function drawMine(arr){
+        var elem = document.getElementById('my-roster')
+        var myTemplate = ''
+        for (var i = 0; i < arr.length; i++) {
+          var player = arr[i];
+          
+          myTemplate += 
+          `
+          <div class="col-xs-3 text-center players">
+            <img src="${player.playerImg}" alt="">
+            <h3>${player.firstName} ${player.lastName}</h3>
+            <h4>${player.team}</h4>
+            <h4>${player.position}</h4>
+            <button onclick="app.controllers.playerController.removeFromRoster(${player.id})">Remove from Roster</button>
+          </div>
+          ` 
+        }
+    
+      
+      elem.innerHTML = myTemplate
+      }
+
+
+}
---
+++ app/nfl/players-service.js
@@ -0,0 +1,118 @@
+var PlayerService = function(callback){
+    var playersData = [];
+    var filteredPlayers = {}
+    var myRoster = []
+      
+    function formatResults(arr){
+      var set = 1
+      var subArr = []
+      for (var i = 0; i < arr.length; i++) {
+        var elem = arr[i];
+        if(subArr.length >= 20){
+          filteredPlayers[set]= subArr
+          set ++
+          subArr = []
+          subArr.push(elem)
+        }else{
+          subArr.push(elem)
+        } 
+      }
+      if(subArr.length > 0 && set === 1){
+        filteredPlayers[set]= subArr
+      }
+      else{
+        filteredPlayers[set++] = subArr
+      }
+      console.log(filteredPlayers)
+      return filteredPlayers
+    }
+    this.addToRoster = function (id, cb) {
+      // debugger
+      for (var i = 0;i < playersData.length; i++) {
+        var player = playersData[i]
+        if (player.id == id) {          
+          if(!myRoster.includes(player)){
+            myRoster.push(player)        
+          }else{    
+            // debugger
+            for (var j = 0; j < myRoster.length; j++) {
+              var myPlayer = myRoster[j];
+              myRoster.splice(j,1, myPlayer)
+            }
+          }
+        }      
+      }console.log(myRoster)
+      cb(myRoster)     
+    }
+    
+    this.getPlayersByTeam = function(teamName){
+      filteredPlayers = {}
+      return formatResults(playersData.filter(function(player){
+        if(player.team.toLowerCase() == teamName.toLowerCase()){
+          return true;
+        }
+      }));
+      tempArr = []
+    }
+    
+    this.getPlayersByPosition = function(position){
+      filteredPlayers = {}
+      return formatResults(playersData.filter(function(player){
+        if(player.position.toLowerCase() == position.toLowerCase()){
+          return true;
+        }
+      }));
+    }
+    this.getPlayersByName = function(lastName){
+      filteredPlayers = {}
+      return formatResults(playersData.filter(function(player){
+        if(player.lastName.toLowerCase() == lastName.toLowerCase()){
+          return true;
+        }
+      }));
+    }
+    this.getMyRoster = function () {
+     
+      return JSON.parse(JSON.stringify(myRoster))
+    }
+    
+    function loadPlayersData(){
+      
+      //Lets check the localstorage for the data before making the call.
+      //Ideally if a user has already used your site 
+      //we can cut down on the load time by saving and pulling from localstorage 
+      
+      var localData = localStorage.getItem('playersData');
+      if(localData){
+        playersData = JSON.parse(localData);
+        console.log(playersData)
+      	return callback(); 
+      	//return will short-circuit the loadPlayersData function
+      	//this will prevent the code below from ever executing
+      }
+      
+      var url = "https://bcw-getter.herokuapp.com/?url=";
+      var endpointUri = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
+      var apiUrl = url + encodeURIComponent(endpointUri);
+    
+        $.getJSON(apiUrl, function(data){
+          playersData = data.body.players.map(function(player){
+            return {
+              firstName: player.firstname,
+              lastName: player.lastname,
+              position: player.position,
+              team: player.pro_team,
+              playerImg: player.photo,
+              id: player.id
+            }
+          });
+          console.log('Player Data Ready')
+          console.log('Writing Player Data to localStorage')
+          localStorage.setItem('playersData', JSON.stringify(playersData))
+          console.log('Finished Writing Player Data to localStorage')
+          callback()
+          console.log(playersData)
+        });
+    }	
+loadPlayersData(); //call the function above every time we create a new service
+} 
